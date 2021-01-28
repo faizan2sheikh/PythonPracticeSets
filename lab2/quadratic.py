cf73c6af432b1db8126e46708b9e994e6ba06860
faizan2sheikh <64231402+faizan2sheikh@users.noreply.github.com>
2021-01-28 21:57:59 +0500

    Add files via upload

---
+++ lab2/quadratic.py
@@ -0,0 +1,32 @@
+##a = int(input("Enter value of a :"))
+##b = int(input("Enter value of b :"))
+##c = int(input("Enter value of c :"))
+
+##val1 = (-b+(b**2 - 4*a*c)**0.5)/(2*a)
+##val2 = (-b-(b**2 - 4*a*c)**0.5)/(2*a)
+
+##print("The values are ",val1,"&",val2)
+
+
+##x = "COMPUTERPROGRAMMING"
+##print(x[3:8])
+##print(x[11:16])
+##print(x[8:15])
+##print(x[0:8])
+##print(x["****************"])
+##print(x[7:2:-1])
+##print(x[15:10:-1])
+##print(x[14:7:-1])
+##print(x[7::-1])
+
+
+##x = "COMPUTERS"
+##print(x[0:9])
+##print(x[1:9])
+##print(x[2:9])
+##print(x[3:9])
+##print(x[4:9])
+##print(x[5:9])
+##print(x[6:9])
+##print(x[7:9])
+##print(x[8:9])
